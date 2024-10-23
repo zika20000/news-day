5ccd308de852bc6f625412f1eaeb73f6e66e1a74
zika <zika01200879280@gmail.com>
2024-10-23 23:04:11 +0300

    This is updata newv branch news day

---
+++ lib/viwe/newsday.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class newsday extends StatelessWidget {
+  const newsday({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
